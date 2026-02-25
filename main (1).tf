ece942d917e96b20d4eb437be92d3733a8e2e0ed
Faizan Mansuri <faizanahmedmansuri@gmail.com>
2026-02-26 04:18:48 +0530

    Initial Commit

---
+++ main (1).tf
@@ -0,0 +1,81 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+    name = "eks-cluster-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Effect = "Allow"
+            Principal = {
+            Service = "eks.amazonaws.com"
+            }
+            Action = [
+            "sts:AssumeRole",
+            "sts:TagSession"
+            ]
+        }
+        ]
+    })
+    }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+    role       = aws_iam_role.eks_cluster_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+    name     = "example-eks-cluster"
+    role_arn = aws_iam_role.eks_cluster_role.arn
+    version  = "1.29"
+
+vpc_config {
+        subnet_ids = [
+        "subnet-0173c2a6d326a5894",
+        "subnet-0a8326480d6a7ae22"
+        ]
+    }
+
+    depends_on = [
+        aws_iam_role_policy_attachment.eks_cluster_policy
+    ]
+    }
+
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Essential policies for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
